8be7e9210862c93826ab804eeb502d6dfd699deb
Calvin Zhu <apexchu@hotmail.com>
2022-05-27 17:52:28 +0800

    Create CrossCompile.cmake

---
+++ CrossCompile.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(TOOLCHAIN_PATH /home/apexchu/Desktop/buildroot/output/host/mips64-buildroot-linux-uclibc/sysroot)
+set(CMAKE_C_COMPILER mips64-linux-gcc)
+set(CMAKE_CXX_COMPILER mips64-linux-g++)

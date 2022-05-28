5d7bebf79272ec91793862ea67f067b815b33e83
Calvin Zhu <apexchu@hotmail.com>
2022-05-28 15:22:08 +0800

    Update CrossCompile.cmake

--- CrossCompile.cmake
+++ CrossCompile.cmake
@@ -6,3 +6,15 @@ set(CMAKE_C_COMPILER "mips64-linux-gcc")
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER "mips64-linux-g++")
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+#指定交叉编译环境安装目录... 
+SET(CMAKE_FIND_ROOT_PATH "/home/apexchu/Desktop/buildroot/output/build")
+
+#从来不在指定目录下查找工具程序
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+#只在指定目录下查找库文件
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+
+#只在指定目录下查找头文件
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)

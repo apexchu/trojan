69510bbdded9e1078223376d572b3f92b5b8c814
Calvin Zhu <apexchu@hotmail.com>
2022-05-28 15:53:19 +0800

    Update CrossCompile.cmake

--- CrossCompile.cmake
+++ CrossCompile.cmake
@@ -2,9 +2,9 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(triple mips64-linux)
 
 set(TOOLCHAIN_PATH "/home/apexchu/Desktop/buildroot/output/host/mips64-buildroot-linux-uclibc/sysroot")
-set(CMAKE_C_COMPILER "mips64-linux-gcc")
+set(CMAKE_C_COMPILER "/home/apexchu/Desktop/buildroot/output/host/bin/mips64-linux-gcc")
 set(CMAKE_C_COMPILER_TARGET ${triple})
-set(CMAKE_CXX_COMPILER "mips64-linux-g++")
+set(CMAKE_CXX_COMPILER "/home/apexchu/Desktop/buildroot/output/host/bin/mips64-linux-g++")
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
 #指定交叉编译环境安装目录... 
